5d74481c64ea77b2f0652b4e09f8da0808a21623
Artem Zhukov <artzhuchka@gmail.com>
2014-01-27 10:44:50 +0400

    test added

--- test/builder.js
+++ test/builder.js
@@ -4,7 +4,7 @@ var expect = require('expect.js');
 var configBuilder = require('../lib');
 
 describe('ConfigBuilder', function() {
-	it('test exported singleton', function() {
+	it('exported singleton simle config', function() {
 		configBuilder.register({
 			name: 'test',
 			config: {
@@ -25,5 +25,57 @@ describe('ConfigBuilder', function() {
 			c: null,
 			d: 100500
 		});
+	});
+
+	it('config inheritance', function() {
+		configBuilder.register({
+			name: 'parent',
+			config: {
+				a: 1,
+				b: '12',
+				c: null,
+				d: function(config) {
+					return 100500;
+				}
+			}
+		});
+
+		configBuilder.register({
+			parent: 'parent',
+			name: 'children',
+			config: {
+				a: 2,
+				e: '22'
+			}
+		});
+
+		var config = configBuilder.get('children');
+
+		expect(config).to.eql({
+			a: 2,
+			b: '12',
+			c: null,
+			d: 100500,
+			e: '22'
+		});
+	});
+
+	it('function field', function() {
+		configBuilder.register({
+			name: 'funcConfig',
+			config: {
+				f: configBuilder.func(function(a, b, c) {
+					return a + b + c;
+				})
+			}
+		});
+
+		var config = configBuilder.get('funcConfig');
+
+		expect(config.f).to.be.a('function');
+
+		var res = config.f(1, 2, 3);
+
+		expect(res).to.be(6);
 	});
 });
